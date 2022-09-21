7c29b0a2f23e04e528ee32f25c8b1d19f7050f25
adevezin <adrianldevezin@mailfence.com>
2022-09-21 14:51:12 -0400

    Disable https redirect for now

--- src/main/kotlin/org/empowrco/plugins/HTTP.kt
+++ src/main/kotlin/org/empowrco/plugins/HTTP.kt
@@ -3,22 +3,21 @@ package org.empowrco.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.cachingheaders.CachingHeaders
-import io.ktor.server.plugins.httpsredirect.HttpsRedirect
 
 fun Application.configureHTTP() {
     install(CachingHeaders) {
-        options { call, outgoingContent ->
+        options { _, outgoingContent ->
             when (outgoingContent.contentType?.withoutParameters()) {
                 else -> null
             }
         }
     }
-    if (!System.getenv("DEBUG").toBoolean()) {
-        install(HttpsRedirect) {
-            // The port to redirect to. By default, 443, the default HTTPS port.
-            sslPort = 443
-            // 301 Moved Permanently, or 302 Found redirect.
-            permanentRedirect = true
-        }
-    }
+    /*    if (!System.getenv("DEBUG").toBoolean()) {
+            install(HttpsRedirect) {
+                // The port to redirect to. By default, 443, the default HTTPS port.
+                sslPort = 443
+                // 301 Moved Permanently, or 302 Found redirect.
+                permanentRedirect = true
+            }
+        }*/
 }
