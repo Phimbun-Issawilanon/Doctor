88a6af629c25a2906b1af56b63909227afa4c8ee
adevezin <adrianldevezin@mailfence.com>
2022-09-21 14:16:42 -0400

    Make NoValidExecutor a data class

--- models/src/main/kotlin/org/empowrco/doctor/models/ExecutorResponse.kt
+++ models/src/main/kotlin/org/empowrco/doctor/models/ExecutorResponse.kt
@@ -2,5 +2,5 @@ package org.empowrco.doctor.models
 
 sealed interface ExecutorResponse
 open class Error(override val message: String) : ExecutorResponse, RuntimeException(message)
-class NoValidExecutor(language: String) : Error(language)
+data class NoValidExecutor(private val language: String) : Error(language)
 data class Success(val output: String) : ExecutorResponse
