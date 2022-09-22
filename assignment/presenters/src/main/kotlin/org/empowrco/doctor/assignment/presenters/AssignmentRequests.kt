ccd539ca3c6288c0895a39511d984ecb0c9e3c64
adevezin <adrianldevezin@mailfence.com>
2022-09-22 14:32:39 -0400

    Support no referenceId

--- assignment/presenters/src/main/kotlin/org/empowrco/doctor/assignment/presenters/AssignmentRequests.kt
+++ assignment/presenters/src/main/kotlin/org/empowrco/doctor/assignment/presenters/AssignmentRequests.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RunRequest(
-    val referenceId: String,
+    val referenceId: String?,
     val language: String,
     val code: String,
 )
