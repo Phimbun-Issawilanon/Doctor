7cabe1b008ff991f495cbecf61de3f2fd9b03e3e
adevezin <adrianldevezin@mailfence.com>
2022-11-13 09:23:04 -0500

    Remove debug code

--- tester/api/src/main/kotlin/org/empowrco/doctor/tester/api/TesterRouting.kt
+++ tester/api/src/main/kotlin/org/empowrco/doctor/tester/api/TesterRouting.kt
@@ -2,10 +2,10 @@ package org.empowrco.doctor.tester.api
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
-import org.empowrco.doctor.tester.presenters.ExecuteTestRequest
 import org.empowrco.doctor.tester.presenters.TesterPresenter
 import org.koin.ktor.ext.inject
 
@@ -13,39 +13,7 @@ fun Application.testerRouting() {
     val presenter: TesterPresenter by inject()
     routing {
         get("test") {
-            val result = presenter.test(
-                ExecuteTestRequest(
-                    language = "swift",
-                    tests = """import XCTest
-@testable import trials
-
-final class trialsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(trials().text, "Hello, Worlfd!")
-    }
-    
-    func testMe() throws {
-        XCTAssertEqual(tester(), 0)
-    }
-}
-""",
-                    code = """public struct trials {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
-
-
-func tester() -> Int {
-    return 0
-}
-"""
-                )
-            )
+            val result = presenter.test(call.receive())
             call.respond(result)
         }
     }
