598a536de9ed90d954463bc7c954505f520043e9
adevezin <adrianldevezin@mailfence.com>
2023-03-17 13:28:07 -0400

    Change test api to post

--- tester/api/src/main/kotlin/org/empowrco/doctor/tester/api/TesterApi.kt
+++ tester/api/src/main/kotlin/org/empowrco/doctor/tester/api/TesterApi.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import org.empowrco.doctor.tester.presenters.TesterPresenter
 import org.koin.ktor.ext.inject
@@ -12,7 +12,7 @@ import org.koin.ktor.ext.inject
 fun Application.testerApi() {
     val presenter: TesterPresenter by inject()
     routing {
-        get("test") {
+        post("test") {
             val response = presenter.test(call.receive())
             call.respond(response)
         }
