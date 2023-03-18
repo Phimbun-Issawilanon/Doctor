5eee4c9b94c25eb3d8eb25e71f9fb5fa6884fcd5
adevezin <adrianldevezin@mailfence.com>
2023-03-18 19:06:25 -0400

    Remove authPost from DownloadApi

--- download/api/src/main/kotlin/org/empowrco/doctor/download/api/DownloadApi.kt
+++ download/api/src/main/kotlin/org/empowrco/doctor/download/api/DownloadApi.kt
@@ -1,18 +1,19 @@
 package org.empowrco.doctor.download.api
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.request.receive
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import org.empowrco.doctor.download.presenters.DownloadPresenter
 import org.empowrco.doctor.download.presenters.DownloadRequest
-import org.empowrco.doctor.utils.routing.authPost
 import org.koin.ktor.ext.inject
 
 fun Application.downloadApi() {
     routing {
         val viewModel: DownloadPresenter by inject()
-        authPost("/download") {
-            val request = it.receive<DownloadRequest>()
+        post("/download") {
+            val request = call.receive<DownloadRequest>()
             viewModel.download(request)
         }
     }
