113db77529530e8f8e0da5313230d8c0b5e49a65
adevezin <adrianldevezin@mailfence.com>
2023-03-18 19:06:19 -0400

    Remove authPost from RunApi

--- run/api/src/main/kotlin/org/empowrco/doctor/run/api/RunApi.kt
+++ run/api/src/main/kotlin/org/empowrco/doctor/run/api/RunApi.kt
@@ -1,17 +1,20 @@
 package org.empowrco.doctor.run.api
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import org.empowrco.doctor.run.presenters.RunPresenter
-import org.empowrco.doctor.utils.routing.authPost
 import org.koin.ktor.ext.inject
 
 fun Application.runApi() {
     val presenter: RunPresenter by inject()
     routing {
-        authPost("/run") {
-            presenter.run(it.receive())
+        post("/run") {
+            val response = presenter.run(call.receive())
+            call.respond(response)
         }
     }
 }
